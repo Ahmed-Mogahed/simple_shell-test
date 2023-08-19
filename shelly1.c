ff14b3e6114b00cfa00cf1d494936899828a95e1
Ahmed Ayman <111667388+Ahmed-Mogahed@users.noreply.github.com>
2023-08-19 19:10:47 +0300

    Update shelly1.c

--- shelly1.c
+++ shelly1.c
@@ -11,7 +11,7 @@ int main(__attribute__((unused)) int argc, char **argv)
     char *input, **cmd;
     int counter = 0, status = 1;
 
-    // Handle optional reading from a file (argv[1])
+
     if (argv[1] != NULL)
         read_file(argv[1], argv);
 
@@ -21,34 +21,33 @@ int main(__attribute__((unused)) int argc, char **argv)
     {
         counter++;
         
-        // Display prompt if shell is interactive
+
         if (isatty(STDIN_FILENO))
             shelly_prompt();
 
-        // Read user input
+
         input = shelly_getline();
 
         if (input[0] == '\0')
         {
-            continue; // Ignore empty input
+            continue;
         }
 
         history(input);
         cmd = shelly_parse_cmd(input);
 
-        // Check if input is the "exit" built-in command
         if (_strcmp(cmd[0], "exit") == 0)
         {
             exit_builtin(cmd, counter, argv);
         }
-        // Check and handle other built-in commands
+
         else if (check_builtin(cmd) == 0)
         {
             handle_builtin(cmd, counter);
         }
         else
         {
-            // Handle external commands
+
             check_cmd(cmd, input, counter, argv);
         }
 
